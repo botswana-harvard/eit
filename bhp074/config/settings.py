0be1003f8da602ef2aa5eeed3f6026accb033d2a
Florence Chilisa <fchilisa@bhp.org.bw>
2015-04-14 09:40:24 +0200

    allowed hosts

--- bhp074/config/settings.py
+++ bhp074/config/settings.py
@@ -96,6 +96,10 @@ else:
         },
     }
 
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'eit', 'eit.bhp.org.bw', ]
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
