7f8f2071acfdd3cc67756ce6508c34d94cb11e4f
Leslie <leslie.kep@gmail.com>
2018-03-31 10:57:35 +0200

    update signal

--- eit/apps/eit_maternal/models/signals.py
+++ eit/apps/eit_maternal/models/signals.py
@@ -8,7 +8,10 @@ from .maternal_consent import MaternalConsent
 @receiver(post_save, weak=False, dispatch_uid='post_save_register_infants')
 def post_save_register_infants(sender, instance, raw, created, using, **kwarg):
     if isinstance(instance, MaternalPostReg):
-        instance.post_save_register_infants(created)
+        i_indicator = instance.live_infants_to_register
+        while i_indicator > 0:
+            instance.post_save_register_infants(created, i_indicator)
+            i_indicator = i_indicator - 1
 
 
 @receiver(post_save, weak=False, dispatch_uid='update_registered_subject_on_post_save')
